de688ee4baf4cefceab885c7294d2d613f42742f
Hash Lin <coosos810609@gmail.com>
2021-05-20 21:51:48 +0800

    fix lint

--- import.py
+++ import.py
@@ -17,8 +17,7 @@ firebase_admin.initialize_app(cred, {
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-    }
-
+}
 
 with open('storage/import.csv') as csvfile:
     rows = iter(csv.reader(csvfile))
